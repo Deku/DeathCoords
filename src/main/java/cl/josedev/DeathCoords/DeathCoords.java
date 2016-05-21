0425628814b42a692c8aca024ad27032a53d57a3
Jose Gonzalez <maangx@gmail.com>
2016-05-21 11:04:27 -0400

    Add command /guia

--- src/main/java/cl/josedev/DeathCoords/DeathCoords.java
+++ src/main/java/cl/josedev/DeathCoords/DeathCoords.java
@@ -1,9 +1,26 @@
 package cl.josedev.DeathCoords;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class DeathCoords extends JavaPlugin {
 
+	public Map<UUID, Location> lastDeath = new HashMap<UUID, Location>();
+	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new DeathListener(this), this);
@@ -14,5 +31,47 @@ public class DeathCoords extends JavaPlugin {
 		
 	}
 	
-	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("guia")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+				return true;
+			}
+			Player player = (Player) sender;
+			
+			if (!lastDeath.containsKey(player.getUniqueId())) {
+				player.sendMessage(ChatColor.RED + "No tienes ubicacion de muerte!");
+				return true;
+			}
+			
+			// Player doesn't have full inventory
+			if (player.getInventory().firstEmpty() > -1) {
+				Location loc = lastDeath.get(player.getUniqueId());
+				ItemStack compass = new ItemStack(Material.COMPASS);
+				ItemMeta meta = compass.getItemMeta();
+				List<String> lore = new ArrayList<String>();
+				lore.add("Mundo: " + loc.getWorld().getName());
+				lore.add("X: " + loc.getBlockX());
+				lore.add("Y: " + loc.getBlockY());
+				lore.add("Z: " + loc.getBlockZ());
+				
+				meta.setDisplayName("Ubicacion de tu muerte");
+				meta.setLore(lore);
+				compass.setItemMeta(meta);
+				
+				player.setCompassTarget(loc);
+				player.getInventory().addItem(compass);
+				player.sendMessage(ChatColor.GREEN + "Se te ha entregado una brujula indicando tu ubicacion de muerte.");
+				player.sendMessage(ChatColor.GREEN + "Esta desaparecera cuando llegues al lugar.");
+			} else {
+				player.sendMessage(ChatColor.RED + "No tienes suficiente espacio en tu inventario!");
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
 }
--- src/main/java/cl/josedev/DeathCoords/DeathListener.java
+++ src/main/java/cl/josedev/DeathCoords/DeathListener.java
@@ -1,12 +1,17 @@
 package cl.josedev.DeathCoords;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 
-import net.md_5.bungee.api.ChatColor;
 
 public class DeathListener implements Listener {
 
@@ -16,24 +21,64 @@ public class DeathListener implements Listener {
 		this.plugin = pl;
 	}
 	
-	@EventHandler
+	@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		try {
 			Player player = event.getEntity();
 			
 			if (player.isOnline()) {
 				Location loc = event.getEntity().getLocation();
+				plugin.lastDeath.put(player.getUniqueId(), loc);
 				String message = ">> " + player.getName()
-									+ ", moriste en las coordenadas "
-									+ "x: " + loc.getBlockX()
-									+ ", y: " + loc.getBlockY()
-									+ ", z: "+loc.getBlockZ();
+									+ ", moriste en: "
+									+ "[Mundo] " + loc.getWorld().getName()
+									+ " - [X] " + loc.getBlockX()
+									+ " - [Y] " + loc.getBlockY()
+									+ " - [Z] "+loc.getBlockZ();
 				
 				player.sendMessage(ChatColor.AQUA + message);
 			}
 		}
 		catch (Exception ex) {
 			this.plugin.getLogger().warning("Couldn't handle death event. Error: " + ex.getMessage());
+		}
+	}
+	
+	@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
+	public void onPlayerReachedDeathPoint(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
+		
+		if (plugin.lastDeath.containsKey(player.getUniqueId()))
+		{
+			Location deathPoint = plugin.lastDeath.get(player.getUniqueId());
+			Location loc = event.getTo();
+			
+			if (loc.distance(deathPoint) <= 5.0) {
+				plugin.lastDeath.remove(player.getUniqueId());
+				
+				// Remove guide compass
+				for (ItemStack item : player.getInventory()) {
+					if (item != null) {
+						if (item.getType().equals(Material.COMPASS)) {
+							if (item.getItemMeta().getDisplayName() == "Ubicacion de tu muerte"
+									&& item.getItemMeta().getLore().size() > 0) {
+								player.getInventory().remove(item);
+							}
+						}
+					}
+				}
+				
+				player.setCompassTarget(new Location(player.getWorld(), 0, 0, 0));
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerDisconnect(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		
+		if (plugin.lastDeath.containsKey(player.getUniqueId())) {
+			plugin.lastDeath.remove(player.getUniqueId());
 		}
 	}
 }
